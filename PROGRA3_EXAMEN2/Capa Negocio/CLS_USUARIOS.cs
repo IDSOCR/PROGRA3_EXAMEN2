400a0d9c893ea3ee9a2f5daa2a416f066d3c7c4b
IDSOCR <isolera@utlook.com>
2024-07-20 22:46:26 -0600

    Agregar archivos de proyecto.

---
+++ PROGRA3_EXAMEN2/Capa Negocio/CLS_USUARIOS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Web;
+
+namespace PROGRA3_EXAMEN2.Capa_Negocio
+{
+    public class CLS_USUARIOS
+    {
+    
+        public int id { get; set; }
+
+        public string usuario { get; set; }
+
+        public string clave { get; set; }
+
+
+    }
+}
---
+++ PROGRA3_EXAMEN2/Presentacion/Loguin.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PROGRA3_EXAMEN2.Presentacion
+{
+    public partial class Loguin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
